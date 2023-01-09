9e12bb38bca0eb076eb313ccad2d362e826cd0b5
alexisForman <111308007+alexisForman@users.noreply.github.com>
2023-01-09 19:50:36 +0000

    Initial commit

---
+++ main.rb
@@ -0,0 +1,6 @@
+puts 'enter rectangle_length: '
+length = gets.chomp
+puts 'enter rectangle_width: '
+width = gets.chomp
+rectangle_area = length.to_i*width.to_i
+print "The area of your rectanlge is: #{rectangle_area}"
