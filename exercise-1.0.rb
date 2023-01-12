e0b0b7af7507b82c614c33ebfeaf4b333d7016cc
alexisForman <111308007+alexisForman@users.noreply.github.com>
2023-01-12 12:23:35 +0000

    answers so far

---
+++ exercise-1.0.rb
@@ -0,0 +1,68 @@
+#Number
+puts 1
+p 1
+
+#String
+puts "Hello World!"
+p "Hello World"
+
+#Boolean
+
+#Hash
+
+#Array
+puts [1,3,5,7,9,11]
+
+#Symbol
+puts :Name
+p :Name
+
+#Addition 
+puts 3+11
+p 3+11
+
+#Subtraction  
+puts 15-7
+p 15-7
+
+#Division 
+puts 20/2
+p 20/2
+
+#Multiplication  
+puts 13*3
+p 13*3
+
+#Exponent
+puts 3**2
+p 3**2
+
+#Modulus
+puts 15%4
+p 15%4
+
+#Concatenation
+puts "This is "<<"an example of "<<"concatenation."
+p "This is "<<"an example of "<<"concatenation."
+
+#Print the seventh letter of "abcdefghijklmnopqrstuvwxyz"
+
+#Formula s=d/t example
+d = 30
+t = 10
+s = d.to_i/t.to_i
+average_speed = s
+puts "If you travel 30KM in 10MIN, your average speed is: #{average_speed}."
+
+#Give an example of the following operators: =, ==, !=, <, >, <= ,=>
+car="Maserati"
+puts car
+MAKE_YEAR = 2003
+puts MAKE_YEAR
+puts 17 !=18
+puts 1000 < 100000.00
+p 1000 < 100000.00
+#value > 15000.00
+#value <= 65000.00
+#value >=65000.00
+#puts !value
